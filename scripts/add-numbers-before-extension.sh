011bbef21bd5e0051ec5f9d71cf5bf4f0820d4d1
Johntini <ekardian@gmail.com>
2025-01-30 17:53:08 -0400

    Add files via upload

---
+++ scripts/add-numbers-before-extension.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#
+# -- Bash script --
+#  github: https://github.com/ekardian
+#
+#  Serialize files with Letter + numbers before the extension
+#
+#      From           To
+#  +-----------+---------------+
+#  |file-a.txt | file-a-F1.txt |
+#  |file-b.txt | file-b-F2.txt |
+#  +-----------+---------------+
+#
+# Try creating this files and test the script:
+#
+# $ mkdir testing
+# $ cd testing
+# $ touch file-{a..b}.txt
+#
+# How to use
+#
+# Step 1
+# Copy the script add-numbers-before-extension.sh
+# to the directory where you want rename to serialize files.
+#
+# Step 2
+# $ chmod +x add-numbers-before-extension.sh
+# $ ./add-numbers-before-extension.sh
+#
+echo ""
+echo -e "\e[1;33m==========================================\e[0m"
+echo -e "\e[1;33mWill be renamed from FILE.extension to\e[0m"
+echo -e "\e[1;33mFILE-(letter)(number increased).extension\e[0m"
+echo -e  "\e[1;33m==========================================\e[0m"
+echo ""
+echo -e "\e[1;33mInital letter for serial like (A,F,Doc,etc) or leave blank for write only numbers:\e[0m "; read -r letter
+echo -e "\e[1;33mWhat number to start enumerating:\e[0m ";  read -r i
+if [ "serial-files.txt" ]; then rm -f serial-files.txt -f; fi
+for f in *
+  do
+    if [ "$f" = "add-numbers-before-extension.sh" ]
+    then
+      continue
+    fi
+    ext=${f##*.}
+    basename=${f%.*}
+    t=$((i++))
+    mv -n -- "$f" "$basename-$letter$t.$ext"
+   echo "$letter$t" >> serial-files.txt
+done
+echo ""
+echo -e "\e[1;33mRERMINADO\e[0m"
+echo ""
+echo -e "\e[1;33mJoin your serialized files: $ cat serial-files.txt /other/serial-files.txt > all-files.txt\e[0m"
+echo ""
---
+++ scripts/add-numbers-before-filename.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#
+# -- Bash script --
+#  github: https://github.com/ekardian
+#
+#  Serialize files with Letter + numbers before the file name
+#
+#      From           To
+#  +-----------+---------------+
+#  |file-a.txt | F1-file-a.txt |
+#  |file-b.txt | F1-file-b.txt |
+#  +-----------+---------------+
+#
+# Try creating this files and test the script:
+#
+# $ mkdir testing
+# $ cd testing
+# $ touch file-{a..b}.txt
+#
+# How to use
+#
+# Step 1
+# Copy the script add-numbers-before-filename.sh
+# to the directory where you want rename to serialize files.
+#
+# Step 2
+# $ chmod +x add-numbers-before-filename.sh
+# $ ./add-numbers-before-filenam.sh
+#
+echo ""
+echo -e "\e[1;33m==========================================\e[0m"
+echo -e "\e[1;33mWill be renamed from FILE.extension to\e[0m"
+echo -e "\e[1;33m(letter)(number increased)-FILE.extension\e[0m"
+echo -e  "\e[1;33m==========================================\e[0m"
+echo ""
+echo -e "\e[1;33mA Letter for description, example (F, A, Doc, Video, etc ), just enter for leave empty:\e[0m" ; read -r letter
+echo -e "\e[1;33mWhat number to start enumerating:\e[0m"; read -r i
+if [ "serial-files.txt" ]; then rm -f serial-files.txt; fi
+for f in *
+  do
+    if [ "$f" = "add-numbers-before-filename.sh" ]
+    then
+      continue
+    fi
+    t=$((i++))
+    mv "$f" "$letter$t-$f"
+    echo "$letter$t" >> serial-files.txt
+done
+echo ""
+echo -e "\e[1;33mTERMINADO\e[0m"
+echo ""
+echo -e "\e[1;33mJoin you serialzed files: $ cat serial-files.txt /other/serial-files.txt > all-files.txt\e[0m"
+echo ""
+
